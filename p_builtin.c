9c670183fe862c8930a00a5573a0b3fe4c160b7d
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-08-11 00:32:27 +0300

    Create p_builtin.c

---
+++ p_builtin.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * get_builtin - Builtin
+ * @cmd: Cmd
+ * Return: A function pointer of the builtin cmd
+ */
+int (*get_builtin(char *cmd))(data_shell *)
+{
+	builtin_t builtin[] = {
+		{ "env", _env },
+		{ "exit", exit_shell },
+		{ "setenv", _setenv },
+		{ "unsetenv", _unsetenv },
+		{ "cd", cd_shell },
+		{ "help", get_help },
+		{ NULL, NULL }
+	};
+	int i;
+
+	for (i = 0; builtin[i].name; i++)
+	{
+		if (_strcmp(builtin[i].name, cmd) == 0)
+			break;
+	}
+
+	return (builtin[i].f);
+}
